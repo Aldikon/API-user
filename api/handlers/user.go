4045366869ecad613e5a25020ae3aeb2deda7bde
Aldikon <aldiyar.gibadatov@gmail.com>
2023-06-15 01:03:56 +0600

    replace method

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -1,9 +1,18 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
+	"path"
+	"strconv"
 
 	"github.com/Aldikon/API-user/model"
+)
+
+type key int
+
+const (
+	UserIDKey key = iota
 )
 
 type user struct {
@@ -16,7 +25,8 @@ func NewUser(us model.UserService) *user {
 	}
 }
 
-func (u *user) User(w http.ResponseWriter, r *http.Request) {
+func (u *user) Users(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	if r.URL.Path != "/user" {
 		http.NotFound(w, r)
 		return
@@ -27,10 +37,30 @@ func (u *user) User(w http.ResponseWriter, r *http.Request) {
 		u.get(w, r)
 	case http.MethodPost:
 		u.post(w, r)
-	case http.MethodPut:
-		u.update(w, r)
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
+func (u *user) User(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// TODO изменить порядок проверки, на метод важнее
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	ctx := context.WithValue(r.Context(), UserIDKey, uint(id))
+
+	switch r.Method {
+	case http.MethodGet:
+		u.getByID(w, r.WithContext(ctx))
 	case http.MethodDelete:
-		u.delete(w, r)
+		u.delete(w, r.WithContext(ctx))
+	case http.MethodPut:
+		u.update(w, r.WithContext(ctx))
 	default:
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	}
