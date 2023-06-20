111d25300d4722c327f6e4f5434662d729d75c12
Aldikon <aldiyar.gibadatov@gmail.com>
2023-06-20 20:30:39 +0600

    add get handler

--- api/handlers/user_get.go
+++ api/handlers/user_get.go
@@ -1,6 +1,38 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
 
-func (u *user) get(w http.ResponseWriter, r *http.Request) {
+	"github.com/Aldikon/API-user/model"
+	"github.com/sirupsen/logrus"
+)
+
+func (u *userHandler) get(w http.ResponseWriter, r *http.Request) {
+	filter := model.FilterUser{}
+
+	filter.Parse(r.URL.Query())
+
+	users, err := u.service.ListWithFilter(r.Context(), filter)
+	if err != nil {
+		logrus.Trace(err)
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(users)
+	if err != nil {
+		logrus.Trace(err)
+		var e *model.ErrorValid
+		if errors.As(err, &e) {
+			writeError(w, http.StatusBadRequest)
+			return
+		}
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
--- api/handlers/user_get_id.go
+++ api/handlers/user_get_id.go
@@ -1,6 +1,33 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
-func (u *user) getByID(w http.ResponseWriter, r *http.Request) {
+	"github.com/sirupsen/logrus"
+)
+
+func (u *userHandler) getByID(w http.ResponseWriter, r *http.Request) {
+	id := getUserID(r.Context())
+	if id == 0 {
+		logrus.Trace("id null")
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
+
+	user, err := u.service.GetByID(r.Context(), id)
+	if err != nil {
+		logrus.Trace(err)
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(user)
+	if err != nil {
+		logrus.Trace(err)
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
