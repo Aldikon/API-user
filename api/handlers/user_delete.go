cc8fa358ff2f76da921b4543427a1dcc995a3548
Aldikon <aldiyar.gibadatov@gmail.com>
2023-06-20 20:24:03 +0600

    add delete handler

--- api/handlers/user_delete.go
+++ api/handlers/user_delete.go
@@ -1,6 +1,23 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
 
-func (u *user) delete(w http.ResponseWriter, r *http.Request) {
+	"github.com/sirupsen/logrus"
+)
+
+func (u *userHandler) delete(w http.ResponseWriter, r *http.Request) {
+	id := getUserID(r.Context())
+	if id == 0 {
+		logrus.Trace("id null")
+		writeError(w, http.StatusNotFound)
+		return
+	}
+
+	err := u.service.Delete(r.Context(), id)
+	if err != nil {
+		logrus.Trace(err)
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
 }
