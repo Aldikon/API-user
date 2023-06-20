e812bc566691349fb53424cf656044158af49895
Aldikon <aldiyar.gibadatov@gmail.com>
2023-06-20 19:34:26 +0600

    rename name table

--- db/migration/000001_init_db.up.sql
+++ db/migration/000001_init_db.up.sql
@@ -1,20 +1,20 @@
-CREATE TYPE "statuses" AS ENUM (
+CREATE TYPE statuses AS ENUM (
   'active',
   'banned',
   'deleted'
 );
 
-CREATE TYPE "genders" AS ENUM (
+CREATE TYPE genders AS ENUM (
   'man',
   'woman'
 );
 
-CREATE TABLE "Users" (
+CREATE TABLE users (
   "id" bigserial PRIMARY KEY,
   "name" varchar,
   "surname" varchar,
   "gender" genders,
   "status" statuses,
   "birth_date" date,
-  "creat_data" date
+  "creat_data" date DEFAULT now()
 );
