a033b64e841b35d88e6fc0b434735ca66d9f2bf4
Aldikon <aldiyar.gibadatov@gmail.com>
2023-06-20 20:29:18 +0600

    add update handler

--- api/handlers/user_put.go
+++ api/handlers/user_put.go
@@ -1,6 +1,46 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
 
-func (u *user) update(w http.ResponseWriter, r *http.Request) {
+	"github.com/Aldikon/API-user/model"
+	"github.com/sirupsen/logrus"
+)
+
+func (u *userHandler) update(w http.ResponseWriter, r *http.Request) {
+	id := getUserID(r.Context())
+	if id == 0 {
+		writeError(w, http.StatusNotFound)
+		return
+	}
+	user := model.UserUpdate{
+		ID: id,
+	}
+
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
+
+	err = json.Unmarshal(data, &user)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
+
+	err = u.service.Update(r.Context(), user)
+	if err != nil {
+		logrus.Trace(err)
+		var e *model.ErrorValid
+		if errors.As(err, &e) {
+			writeError(w, http.StatusBadRequest)
+			return
+		}
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
 }
