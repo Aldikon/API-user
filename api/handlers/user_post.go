bbbfac739a4f783cca6dde83a8a695cc0b540094
Aldikon <aldiyar.gibadatov@gmail.com>
2023-06-20 20:28:56 +0600

    add post handler

--- api/handlers/user_post.go
+++ api/handlers/user_post.go
@@ -1,6 +1,40 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
 
-func (u *user) post(w http.ResponseWriter, r *http.Request) {
+	"github.com/Aldikon/API-user/model"
+	"github.com/sirupsen/logrus"
+)
+
+func (u *userHandler) post(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		logrus.Trace(err)
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
+
+	user := model.CreateUser{}
+	err = json.Unmarshal(data, &user)
+	if err != nil {
+		logrus.Trace(err)
+		writeError(w, http.StatusBadRequest)
+		return
+	}
+
+	err = u.service.Creat(r.Context(), user)
+	if err != nil {
+		logrus.Trace(err)
+		var e *model.ErrorValid
+		if errors.As(err, &e) {
+			writeError(w, http.StatusBadRequest)
+			return
+		}
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
 }
